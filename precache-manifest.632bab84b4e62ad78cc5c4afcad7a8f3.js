1fa07f01505ab47f23df6d9922faea8cd86783ca
Jesus Valentin Mendoza Torres <jmendozat13@gmail.com>
2020-04-20 19:07:43 -0500

    Updates

---
+++ precache-manifest.632bab84b4e62ad78cc5c4afcad7a8f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "867045771b1842e3d3c6f8408cdae7c3",
+    "url": "/labwebview/index.html"
+  },
+  {
+    "revision": "a2ce0b91d0d4b9c90b7d",
+    "url": "/labwebview/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "94dec0ba85f7ec217ad5",
+    "url": "/labwebview/static/js/2.109a5303.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/labwebview/static/js/2.109a5303.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2ce0b91d0d4b9c90b7d",
+    "url": "/labwebview/static/js/main.72da2a61.chunk.js"
+  },
+  {
+    "revision": "ae8c0db05cb288b9231d",
+    "url": "/labwebview/static/js/runtime-main.d9b86f8b.js"
+  }
+]);
