63cc3ea47d4eb1321e3ebec72ee9b23c5a676bf7
Jesus Valentin Mendoza Torres <jmendozat13@gmail.com>
2020-04-21 11:29:17 -0500

    Updates

---
+++ precache-manifest.1fbc0e6aa33767c1b75ff62a1115ea62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64bfdc1415046cefeaacb8fa98d0fb23",
+    "url": "/labwebview/index.html"
+  },
+  {
+    "revision": "f4fd8a8d73c1fb4d832e",
+    "url": "/labwebview/static/css/main.5534856d.chunk.css"
+  },
+  {
+    "revision": "776fbb790058d9fe66bb",
+    "url": "/labwebview/static/js/2.205ad2c9.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/labwebview/static/js/2.205ad2c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4fd8a8d73c1fb4d832e",
+    "url": "/labwebview/static/js/main.ec47e8a9.chunk.js"
+  },
+  {
+    "revision": "ae8c0db05cb288b9231d",
+    "url": "/labwebview/static/js/runtime-main.d9b86f8b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/labwebview/precache-manifest.572d2cf4c0f0dcb2d037087fc28e1563.js"
+  "/labwebview/precache-manifest.1fbc0e6aa33767c1b75ff62a1115ea62.js"
 );
 
 self.addEventListener('message', (event) => {
